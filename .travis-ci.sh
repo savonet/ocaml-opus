9c8a58278ba00f2dee25cbebb4c8a055c11ca04e
Samuel Mimram <smimram@gmail.com>
2019-12-13 14:59:14 +0100

    Build ocaml-ogg.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -11,5 +11,8 @@ fi
 eval `opam config env`
 opam install -q -y ${OPAM_PACKAGES}
 
+# compile ocaml-ogg
+git clone https://github.com/savonet/ocaml-ogg.git
+cd ocaml-ogg && opam pin add . -y && cd ..
 # compile & run tests
 ./bootstrap && ./configure && make && make -C examples test
